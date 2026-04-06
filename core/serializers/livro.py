bf6e69a2dbecc4deaed9e0fb0577d8df94bb990d
kellyalvesp <zkellypatriciaalves14@gmail.com>
2026-04-06 16:08:29 -0300

    feat: criação de dois serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,5 +1,14 @@
 from rest_framework.serializers import ModelSerializer
+
 from core.models import Livro
+
+
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
+
 
 class LivroSerializer(ModelSerializer):
     class Meta:
