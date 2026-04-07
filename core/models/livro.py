a9abb24bf558866f0d9b8308737ea96b0e3e1fa4
kellyalvesp <zkellypatriciaalves14@gmail.com>
2026-04-07 15:03:00 -0300

    feat: inclusão da app de upload e associação de imagens

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,15 +1,26 @@
 from django.db import models
 
+from uploader.models import Image
+
 from .categoria import Categoria
 from .editora import Editora
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    capa = models.ForeignKey(
+        Image,
+        related_name='+',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
