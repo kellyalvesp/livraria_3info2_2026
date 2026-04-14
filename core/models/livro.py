cf934d8c42aa7720633748ea769927e2f40f0042
kellyalvesp <zkellypatriciaalves14@gmail.com>
2026-04-14 15:04:53 -0300

    feat: dump e load de dados

--- core/models/livro.py
+++ core/models/livro.py
@@ -10,7 +10,7 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
     capa = models.ForeignKey(
@@ -21,6 +21,7 @@ class Livro(models.Model):
         blank=True,
         default=None,
     )
+    autores = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.titulo} ({self.quantidade})'
